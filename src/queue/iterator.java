a5786e34b2bcd9c2bd8abf8de00e3a9897042d63
wesleyc1031 <51250479+wesleyc1031@users.noreply.github.com>
2019-06-04 19:30:57 -0400

    Add files via upload

---
+++ src/queue/iterator.java
@@ -0,0 +1,19 @@
+package queue;
+
+import java.util.Iterator;
+
+public class iterator implements Iterator<Integer> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
